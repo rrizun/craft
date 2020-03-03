5531a15d052e4b28448dabef91c48ef1dc23c515
rrizun <randall.rizun@viasat.com>
2020-03-03 13:19:43 -0800

    jdbcHelper

--- src/main/java/craft/ports/CraftPort.java
+++ src/main/java/craft/ports/CraftPort.java
@@ -38,4 +38,16 @@ public class CraftPort {
     return new HelloResponse(service.hello(name));
   }
 
+  // GET /now
+  @GetMapping("/now")
+  public String now() throws Exception {
+    return service.now();
+  }
+
+  // // GET /users
+  // @GetMapping("/users")
+  // public Object users() {
+  // return service.users();
+  // }
+
 }
--- src/main/java/craft/service/CraftService.java
+++ src/main/java/craft/service/CraftService.java
@@ -1,15 +1,28 @@
 package craft.service;
 
+import javax.sql.DataSource;
+
 import org.springframework.stereotype.Service;
 
+import craft.JdbcHelper;
 import craft.LogHelper;
 
 @Service
 public class CraftService {
 
+  private final DataSource dataSource;
+
+  public CraftService(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
   public String hello(String name) {
     log("hello", name);
     return String.format("Hello, %s!", name);
+  }
+
+  public String now() throws Exception {
+    return new JdbcHelper(dataSource).query(String.class, "select now()").iterator().next();
   }
 
   private void log(Object... args) {
